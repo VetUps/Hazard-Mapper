af2dae434fc8b2fca100bf3d7573e45f324e8863
Никита Чернов <sercherufa@gmail.com>
2025-05-20 13:18:27 +0500

    Построена начальная структура проекта

--- streamlit_app.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-pg = st.navigation([st.Page('tracks_load_page.py', title='Загрузка треков'),
-                    st.Page('tracks_viewing_page.py', title='Просмотр существующих треков')])
-pg.run()

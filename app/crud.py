bb4769b102d5d75009561a1084e0f11d0aaaea98
VetUps <sercherufa@gmail.com>
2025-06-18 23:24:50 +0500

    Создание первоначальных CRUD операций с пользователем

---
+++ app/crud.py
@@ -0,0 +1,31 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+def create_user(db: Session, user: schemas.UserCreate):
+    hashed_password = pwd_context.hash(user.password)
+    db_user = models.User(
+        email=user.email,
+        username=user.username,
+        password_hash=hashed_password
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def authenticate_user(db: Session, email: str, password: str):
+    user = get_user_by_email(db, email)
+    if not user:
+        return False
+    if not pwd_context.verify(password, user.password_hash):
+        return False
+    return user
